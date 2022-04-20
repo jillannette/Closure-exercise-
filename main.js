e6e3c3de398eb1a0e01d949bd5b2a6d6bc6b3c51
Jill Arnold <jillarnold2913@gmail.com>
2022-04-20 15:48:06 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+let buyAtStore = function () {
+     let groceries = ["milk", "bread", "eggs"];
+         
+     let listPurchases = function () {
+          for (let i = 0; i < groceries.length; i+= 1) {
+               console.log(groceries[i]);
+          }
+
+     };
+
+     listPurchases();
+     
+     };
+
+     buyAtStore(); //
+     milk     main.js:6 
+     bread    main.js:6 
+     eggs     main.js:6
